276ddfd22fd87efa54c01bcd6cfed780a82897f6
manale el gaamouss <manale.el-gaamouss@ens.uvsq.fr>
2020-04-09 08:18:59 +0200

    implementation DAOFactory

--- ser/src/main/java/pglp_5/ser/DAOFactory.java
+++ ser/src/main/java/pglp_5/ser/DAOFactory.java
@@ -1,8 +1,17 @@
 package pglp_5.ser;
 
 public class DAOFactory {
-	 public static DAO<Personne1> getPersonne1DAO(){
-	        
-	        return new Personne1DAO();
-	 }
+	
+	public static DAO<Personne1> getPersonne1DAO(){ 
+			return  getPersonne1DAO();
+		}
+	
+	public static DAO<Numero_telephone> getNumero_telephoneDAO(){
+		return getNumero_telephoneDAO();
+		}
+	
+    public static DAO<Groupe> getGroupe() {
+		        return getGroupe();
+		        
+		 }
 }
